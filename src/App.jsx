598a50126716fa35295c4ea31409834ae8c0fefc
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-06 12:17:34 +0600

    project installed with react router, tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='w-6xl mx-auto h-screen '>
+      <div className='flex justify-center h-full py-6'>
+        <h2 className='text-4xl font-bold'>Simple CRUD</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
