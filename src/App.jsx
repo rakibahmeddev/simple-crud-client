5ebce578f90438a876956fffb4b1b46ac83f8eaf
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-13 23:22:37 +0600

    fetch data from backend api and then into the second peramiter used method POST, headers, body for json.stringify() then rest of the thing is common

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,22 @@ function App() {
     const email = form.email.value;
     const user = { name, email };
     console.log(user);
+
+    fetch('http://localhost:3000/users',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      form.reset();
+      alert('User added successfully');
+    })
+
   };
 
   return (
