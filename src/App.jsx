474bc0b37cdca7a6f0bda11452d4bbb2df9afc2f
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-13 23:06:24 +0600

    user registration form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-  
-
   return (
-    <div className='w-6xl mx-auto h-screen '>
-      <div className='flex justify-center h-full py-6'>
-        <h2 className='text-4xl font-bold'>Simple CRUD</h2>
+    <div className="w-6xl mx-auto h-screen ">
+      <div className="flex flex-col justify-center py-6">
+        <h2 className="text-4xl font-bold text-center mb-6">Simple CRUD</h2>
+        {/* form start  */}
+        <form>
+          <div className="w-[400px] mx-auto flex flex-col justify-center gap-4">
+            <div className="w-full">
+              <label className="text-black/70" htmlFor="name">
+                Your Name
+              </label>
+              <input
+                className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-indigo-300"
+                type="text"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-black/70" htmlFor="name">
+                Your Email
+              </label>
+              <input
+                className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-indigo-300"
+                type="email"
+                required
+              />
+            </div>
+
+            <input
+              type="submit"
+              value="Add User"
+              className="mt-5 bg-indigo-600 cursor-pointer text-white h-12 w-56 mx-auto px-4 rounded active:scale-95 transition"
+            />
+          </div>
+        </form>
+        {/* form end  */}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
